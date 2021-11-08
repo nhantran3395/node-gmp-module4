92e0bd895ac5b9b27afca32cc0612c27687d2296
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-11-09 04:30:11 +0700

    fix: resolve issue that make validator return true false unexpectedly after each call

--- src/utils/uuid-validator.util.ts
+++ src/utils/uuid-validator.util.ts
@@ -1,9 +1,9 @@
 import { Logger } from "../logger";
 
-const uuidRegExp =
-  /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
-
 const uuidValidator = (uuid: string): boolean => {
+  const uuidRegExp =
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
   return uuidRegExp.test(uuid);
 };
 
