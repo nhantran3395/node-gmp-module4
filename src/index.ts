d426cbf239e8ad2154b0b99c7da0821b26ec4747
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-11-08 23:47:00 +0700

    feat: set up postgres connection and sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import "./configs/dotenv.config";
+import { sequelize } from "./configs/sequelize.config";
 import { userRouter } from "./routes";
 import { Logger } from "./logger";
 import {
@@ -9,13 +10,22 @@ import {
 } from "./middlewares";
 
 const app = express();
-const port = process.env.APPLICATION_PORT_TASK1 ?? 3002;
+const port = process.env.APPLICATION_PORT ?? 3002;
 
 app.use(express.json());
 app.use(morganMiddleware);
 
 app.get("/", function (req, res) {
   res.json({ message: "module 3 application is running" });
+});
+
+app.get("/databaseconnection", async function (req, res, next: NextFunction) {
+  try {
+    await sequelize.authenticate();
+    res.json({ message: "successfully connected to database" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.use("/users", userRouter);
